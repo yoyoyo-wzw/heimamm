864ccc9c59cb849a37c1ac05b7074ceeb86e88e7
yoyoyo-wzw <1055812484@qq.com>
2019-12-13 11:25:48 +0800

    项目结构--路由整合

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-    123
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
---
+++ src/router/router.js
@@ -0,0 +1,17 @@
+// 导包
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// 注册
+Vue.use(VueRouter);
+
+// 路由配置
+const routes = [];
+
+// 实例化
+const router = new VueRouter({
+    routes
+})
+
+// 暴露
+export default router;
