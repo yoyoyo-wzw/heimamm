f0957a1937c2e9062d61940b597ed1e613c10a15
yoyoyo-wzw <1055812484@qq.com>
2019-12-13 13:27:37 +0800

    项目整合--登录布局

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,30 +1,34 @@
 <template>
   <div class="login-box">
-      <div class="from-box"></div>
-      <img class='login-pic' src="../assets/login_banner_ele.png" alt="">
+    <div class="from-box"></div>
+    <img class="login-pic" src="../assets/login_banner_ele.png" alt />
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-    .login-box {
-        height: 100%;
-        display: flex;
-        /* 垂直居中 */
-        align-items: center;
-        /* 左右均分 */
-        justify-content: space-around;
-        background: linear-gradient(225deg,rgba(20,147,250,1),rgba(1,198,250,1));
-    }
-    .login-box .from-box {
-        width: 478px;
-        height: 550px;
-        background-color: #F5F5F5;
-    }
-    .login-box .login-pic{}
+<style lang='less'>
+.login-box {
+  height: 100%;
+  display: flex;
+  /* 垂直居中 */
+  align-items: center;
+  /* 左右均分 */
+  justify-content: space-around;
+  background: linear-gradient(
+    225deg,
+    rgba(20, 147, 250, 1),
+    rgba(1, 198, 250, 1)
+  );
+
+  .from-box {
+    width: 478px;
+    height: 550px;
+    background-color: #f5f5f5;
+  }
+  .login-pic {
+  }
+}
 </style>
